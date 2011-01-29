ebd2925ec257a412278616cfafae6d77ad9e8fe2
Patrick Naubert <jehreg@gmail.com>
2011-01-28 23:09:47 -0500

    Change the maximum project identifier constant

--- lib/clf2/hooks/project_hooks.rb
+++ lib/clf2/hooks/project_hooks.rb
@@ -8,7 +8,7 @@ module Clf2
           response << content_tag(:p,
                                   context[:form].text_field(:name_fr) +
                                   '<br />' +
-                                  content_tag(:em, l(:text_caracters_maximum, Project::MAXIMUM_CHARACTERS_FOR_NAME)))
+                                  content_tag(:em, l(:text_caracters_maximum, Project::IDENTIFIER_MAX_LENGTH)))
           
           response << content_tag(:p, context[:form].text_area(:description_fr, :rows => 5, :class => 'wiki-edit'))
           response << javascript_tag("var wikiToolbar = new jsToolBar($('project_description_fr')); wikiToolbar.draw();")
