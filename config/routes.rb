6fca04764d4bc21708485960d3d945d51d55e7f6
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-04-26 16:43:09 -0600

    static pages for help and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.english 'english', :controller => 'language_switcher', :action => 'english'
   map.french 'french', :controller => 'language_switcher', :action => 'french'
+  map.help '/help', :controller => 'application', :action => 'help'
+  map.aide '/aide', :controller => 'application', :action => 'help'
+  map.contact '/contact', :controller => 'application', :action => 'contact'
 end
--- routes.rb
+++
@@ -1,4 +0,0 @@
-# home '', :controller => 'projects', :action => 'index'
-
-connect 'french', :controller => 'application', :action => 'french'
-connect 'english', :controller => 'application', :action => 'english'
