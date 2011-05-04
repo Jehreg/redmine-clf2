e91980b6eee67e09260a7fb77eac6d9ca819099f
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-05-04 11:59:37 -0600

    flat list instead of tree list for the project jump menu

--- app/helpers/clf2_helper.rb
+++ app/helpers/clf2_helper.rb
@@ -56,4 +56,19 @@ module Clf2Helper
       end
     end
   end
+
+  def project_tree_options_for_select(projects, options = {})
+    s = ''
+    projects.each do |project|
+      tag_options = {:value => project.id}
+      if project == options[:selected] || (options[:selected].respond_to?(:include?) && options[:selected].include?(project))
+        tag_options[:selected] = 'selected'
+      else
+        tag_options[:selected] = nil
+      end
+      tag_options.merge!(yield(project)) if block_given?
+      s << content_tag('option', h(project), tag_options)
+    end
+    s
+  end
 end
